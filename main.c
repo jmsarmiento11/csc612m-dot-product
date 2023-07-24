69698c29d58f37e6e2684434da16f96cc10bc354
jmsarmiento11 <130573289+jmsarmiento11@users.noreply.github.com>
2023-07-24 14:56:59 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,10 @@
 #include <math.h>
 
 extern void SIMDdotProduct(size_t ARRAY_SIZE, float* A, float* B,float* sdot);
+<<<<<<< HEAD
+extern void x86dotProduct(size_t ARRAY_SIZE, float* A, float* B, float* sdot);
+=======
+>>>>>>> f35bcde2a6039e6a935b8c2f556fd38623ff0f03
 
 const int ARRAY_SIZE = 1<<20;
 
@@ -28,11 +32,11 @@ void verifyDotProduct(const float* sdot, const float* expected)
 {
     for (int i = 0; i < ARRAY_SIZE; i++) {
         if (fabs(sdot[i] - expected[i]) > 1e-5) {
-            printf("Verification failed at index %d\n", i);
+            printf("Error found at index %d\n", i);
             return;
         }
     }
-    printf("Verification successful\n");
+    printf("No error.\n");
 }
 
 int main()
@@ -110,7 +114,47 @@ int main()
 
     printf("Dot product result: %f\n", sum);
     printf("SIMD function took %f microseconds for array size %d \n", time_taken, ARRAY_SIZE);
+<<<<<<< HEAD
+
+
+    // --------------------- x86-64 version ----------------------------------
+
+        // initialize  back sdot[i] to 0
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        sdot[i] = 0.0f;
+
+    //flush out cache
+    x86dotProduct(ARRAY_SIZE, A, B, sdot);
+
+    // fill in the host memory with data
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        A[i] = i;
+        B[i] = i;
+        expected[i] = A[i] * B[i]; // compute the expected result on the CPU
+    }
+
+    // Measure execution time
+    start = clock();
+
+    x86dotProduct(ARRAY_SIZE, A, B, sdot);
+
+    // verify the correctness of the dot product
+    verifyDotProduct(sdot, expected);
+
+    // finish up on the CPU side
+    sum = 0;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        sum += sdot[i];
+    }
+
+    end = clock();
+    time_taken = ((double)(end - start)) * 1e6 / CLOCKS_PER_SEC;
+
+    printf("Dot product result: %f\n", sum);
+    printf("x86-64 function took %f microseconds for array size %d \n", time_taken, ARRAY_SIZE);
+=======
     
+>>>>>>> f35bcde2a6039e6a935b8c2f556fd38623ff0f03
     
     
     
